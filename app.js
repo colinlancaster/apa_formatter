25e4d4f885b5fefda0ee7b723ea453e374ba7a24
colinlancaster <lancaster.colin@gmail.com>
2018-03-14 21:18:01 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+"use strict";
+
+(function() {
+  // Types
+    // book
+      // Author
+      // Source Title
+      // Publisher / City / State / Year
+      // Publish Date
+    // magazine
+      // Author
+      // Source Title
+      // Publisher / City / State / Year
+      // Publish Date
+    // newspaper
+      // Author
+      // Source Title
+      // Publisher / City / State / Year
+      // Publish Date
+    // website
+      // Author
+      // Source Title
+      // Publisher / City / State / Year
+      // Publish Date or Access Date
+      // URL
+
+    // journal
+      // Author
+      // Source Title
+      // Publisher / City / State / Year
+      // Publish Date or Access Date
+    // film
+      // Author
+      // Source Title
+      // Publisher / City / State / Year
+      // Publish Date or Access Date
+
+
+})();
