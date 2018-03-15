54c1314c297ec2035a76d988bd7d153f9bcd895c
Colin Lancaster <lancaster.colin@gmail.com>
2018-03-14 23:31:39 -0700

    Created initial prototype

--- app.js
+++ app.js
@@ -1,39 +1,60 @@
 "use strict";
 
 (function() {
-  // Types
-    // book
-      // Author
-      // Source Title
-      // Publisher / City / State / Year
-      // Publish Date
-    // magazine
-      // Author
-      // Source Title
-      // Publisher / City / State / Year
-      // Publish Date
-    // newspaper
-      // Author
-      // Source Title
-      // Publisher / City / State / Year
-      // Publish Date
-    // website
-      // Author
-      // Source Title
-      // Publisher / City / State / Year
-      // Publish Date or Access Date
-      // URL
 
-    // journal
-      // Author
-      // Source Title
-      // Publisher / City / State / Year
-      // Publish Date or Access Date
-    // film
-      // Author
-      // Source Title
-      // Publisher / City / State / Year
-      // Publish Date or Access Date
+  function Citation(author, title, publisher, publisherCity, publisherState, publisherYear,publishedDate, accessedDate, url) {
+    // instantiate like this:
+    // var book = new Citation("James Sullivan", "Your mom"... "November 12", "www.yourmom.com");
+    this.author = author,
+    this.title = title,
+    this.publisher = publisher,
+    this.publisherCity = publisherCity,
+    this.publisherState = publisherState,
+    this.publisherYear = publisherYear,
+    this.publishedDate = publishedDate,
+    this.accessedDate = accessedDate,
+    this.url = url,
+  };
+
+  
+  var book = new Citation();
+
+  // book
+    // Author
+    // Source Title
+    // Publisher / City / State / Year
+    // Publish Date
+
+  // magazine
+    // Author
+    // Source Title
+    // Publisher / City / State / Year
+    // Publish Date
+
+  // newspaper
+    // Author
+    // Source Title
+    // Publisher / City / State / Year
+    // Publish Date
+
+  // website
+    // Author
+    // Source Title
+    // Publisher / City / State / Year
+    // Publish Date or Access Date
+    // URL
+
+  // journal
+    // Author
+    // Source Title
+    // Publisher / City / State / Year
+    // Publish Date or Access Date
+
+  // film
+    // Author
+    // Source Title
+    // Publisher / City / State / Year
+    // Publish Date or Access Date
 
 
 })();
