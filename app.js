f03b9f9587767cb9a82b1331d9f9220954d04ed5
colinlancaster <lancaster.colin@gmail.com>
2018-03-17 15:17:53 -0700

    Form finished. Bare bones functionality finished

--- app.js
+++ app.js
@@ -1,39 +1,34 @@
 "use strict";
 
-(function() {
-  // Types
-    // book
-      // Author
-      // Source Title
-      // Publisher / City / State / Year
-      // Publish Date
-    // magazine
-      // Author
-      // Source Title
-      // Publisher / City / State / Year
-      // Publish Date
-    // newspaper
-      // Author
-      // Source Title
-      // Publisher / City / State / Year
-      // Publish Date
-    // website
-      // Author
-      // Source Title
-      // Publisher / City / State / Year
-      // Publish Date or Access Date
-      // URL
+var main = function() {
+  var getIdVal = function(idName) {
+    var val = document.getElementById(idName).value;
+    return val;
+  }
 
-    // journal
-      // Author
-      // Source Title
-      // Publisher / City / State / Year
-      // Publish Date or Access Date
-    // film
-      // Author
-      // Source Title
-      // Publisher / City / State / Year
-      // Publish Date or Access Date
+  var firstName = getIdVal("firstName");
+  var middleInitial = getIdVal("middleInitial");
+  var lastName = getIdVal("lastName");
+  var articleName = getIdVal("articleName");
+  var publisherName = getIdVal("publisherName");
+  var url = getIdVal("url");
+  var publisherCity = getIdVal("publisherCity");
+  var publisherState = getIdVal("publisherState");
+  var publisherYear = getIdVal("publisherYear");
+  var datePublished = getIdVal("datePublished");
+  var emphasisElement = document.createElement("EM");
+  var textNode = document.createTextNode(publisherName);
+  var emphasizedPublisherName = emphasisElement.appendChild(textNode);
+  console.log("This is the emphasiszedPublisherName" + emphasizedPublisherName);
+
+  var citation = lastName + ", " + firstName[0] + ". " + middleInitial[0] + ". (" + publisherYear + "). " + articleName + ". " + "<em>" + publisherName + "</em>" + ". Retrieved from " + url;
 
 
-})();
+  var completeCitation = document.getElementById("completeCitation");
+  console.log("This is the completeCitation: " + completeCitation);
+  var content = document.createTextNode(citation);
+  completeCitation.append(content);
+
+  console.log(citation);
+  
+}
